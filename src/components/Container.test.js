ce628e732224e21d968414e40883a37abc18903e
Sebastian Guerrero <wesdell17@gmail.com>
2023-04-07 09:41:45 -0500

    testing initial components App and Container

--- src/components/Container.test.js
+++ src/components/Container.test.js
@@ -1,9 +1,8 @@
 import { render } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
-import App from './App';
+import Container from './Container';
 
-it("render Container component", () => {
-  const {container} = render(<App />)
+it('render component', () => {
+  const {container} = render(<Container />)
   expect(container).toHaveTextContent('Container')
 })
-
