dc96f20e12646aa6d88a68535d29d485773bf4de
Sebastian Guerrero <wesdell17@gmail.com>
2023-04-16 12:53:28 -0500

    Modified file and folder structure

--- src/components/Container.test.js
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect';
-import Container from './Container';
-
-it('render component', () => {
-  render(<Container />)
-  const element = screen.getByRole("main")
-  expect(element).toBeInTheDocument()
-})
